acab0651f45ede579c438f3806ffe6513d3ce470
yuuangzhang <yuuangzhang@tencent.com>
2022-04-01 18:54:10 +0800

    feat: update publicPath

--- children/config.ts
+++ children/config.ts
@@ -2,7 +2,8 @@ import type { UserConfig } from 'ssr-types'
 
 const userConfig: UserConfig = {
   prefix: '/children',
-  clientPrefix: '/main/children'
+  clientPrefix: '/main/children',
+  publicPath: 'http://localhost:3000'
 }
 
 export { userConfig }
